6bc6dcecf38366b75d663b51bdffc739204c150c
kobi97 <kobi97@ridi.com>
2017-09-25 12:27:09 +0900

    add cron master util

---
+++ lib/Cron/Interfaced/CronInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ridibooks\Platform\Common\Cron\Interfaced;
+
+interface CronInterface
+{
+	public function isTimeToRun(\DateTime $last_executed_datetime): bool;
+	public function run(): bool;
+}
