2de02c30685f7fef46a0492bffecaf2a9e584eaa
aeyeong <iiogmgo@ridi.com>
2018-11-28 16:32:26 +0900

    SQSUtils 구현
    1. AbstractAwsUtils : getClientType 추가 및 기존의 getClient 역할 변경
    2. AwsConfigDto : 네이밍 변경, exportToConnect 구현

--- lib/AbstractAwsUtils.php
+++ lib/AbstractAwsUtils.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Ridibooks\Platform\Common;
 
 use Aws\Exception\AwsException;
-use Ridibooks\Platform\Common\Dto\AwsConfig;
+use Ridibooks\Platform\Common\Dto\AwsConfigDto;
 use Ridibooks\Platform\Common\Exception\MsgException;
 
 abstract class AbstractAwsUtils
@@ -13,26 +13,32 @@ abstract class AbstractAwsUtils
 
     /**
      * AbstractAwsUtils constructor.
-     * @param AwsConfig $aws_config
+     * @param AwsConfigDto $aws_config
      * @throws MsgException
      */
-    public function __construct(AwsConfig $aws_config)
+    public function __construct(AwsConfigDto $aws_config)
     {
         $this->connect($aws_config);
     }
 
-    abstract public static function getClient();
+    abstract protected function getClientType();
 
     /**
-     * @param AwsConfig $aws_config
+     * $this->client->method() 접근시 method not found 에러가 뜨게 되므로 $this->getClient()->method() 로 접근하도록
+     * @return mixed
+     */
+    abstract protected function getClient();
+
+    /**
+     * @param AwsConfigDto $aws_config
      * @throws MsgException
      */
-    protected function connect(AwsConfig $aws_config)
+    protected function connect(AwsConfigDto $aws_config)
     {
-        $client_type = self::getClient();
+        $client_type = $this->getClientType();
 
         try {
-            $this->client = new $client_type($aws_config);
+            $this->client = new $client_type($aws_config->exportToConnect());
         } catch (AwsException $e) {
             throw new MsgException($e->getMessage());
         }
---
+++ lib/SQSUtils.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace Ridibooks\Platform\Common;
+
+use Aws\Exception\AwsException;
+use Aws\Sqs\SqsClient;
+use Ridibooks\Platform\Common\Dto\AwsConfigDto;
+use Ridibooks\Platform\Common\Exception\MsgException;
+
+class SQSUtils extends AbstractAwsUtils
+{
+    /** @var string */
+    private $queue_url;
+
+    /**
+     * SQSUtils constructor.
+     * @param string $queue_url
+     * @param AwsConfigDto $aws_config
+     * @throws MsgException
+     */
+    public function __construct(string $queue_url, AwsConfigDto $aws_config)
+    {
+        parent::__construct($aws_config);
+        $this->queue_url = $queue_url;
+    }
+
+    protected function getClient(): SqsClient
+    {
+        return $this->client;
+    }
+
+    protected function getClientType(): string
+    {
+        return SqsClient::class;
+    }
+
+    /**
+     * @param array $attributes
+     * @param string $message
+     * @param int $delay_seconds
+     * @throws MsgException
+     */
+    public function addMessage(array $attributes, string $message, int $delay_seconds = 10): void
+    {
+        $params = [
+            'DelaySeconds' => $delay_seconds,
+            'MessageAttributes' => $attributes,
+            'QueueUrl' => $this->queue_url,
+            'MessageBody' => $message,
+        ];
+
+        try {
+            $this->getClient()->sendMessage($params);
+        } catch (AwsException $e) {
+            throw new MsgException($e->getMessage());
+        }
+    }
+}
