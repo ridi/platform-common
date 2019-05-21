f367c9e4a5aac9b46903ff43b60646b9616c1ac6
문정기 <jerrym@ridi.com>
2019-05-21 12:11:17 +0900

    MsgException -> extends \Exception & copy contents from RidiBaseException

--- lib/Exception/MsgException.php
+++ lib/Exception/MsgException.php
@@ -1,6 +1,35 @@
 <?php
 namespace Ridibooks\Platform\Common\Exception;
 
-class MsgException extends \Ridibooks\Exception\MsgException
+use Ridibooks\Library\SentryHelper;
+
+class MsgException extends \Exception
 {
+    private $should_be_logged = false;
+
+    /**
+     * @param string $message
+     * @param bool $should_be_logged
+     */
+    public function __construct($message = "", $should_be_logged = false)
+    {
+        parent::__construct($message);
+
+        $this->should_be_logged = $should_be_logged;
+
+        // $should_be_logged가 설정되면 Exception 생성 즉시 Sentry Logging
+        if ($should_be_logged) {
+            if (!SentryHelper::triggerSentryException($this)) {
+                trigger_error($this);
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function shouldBeLogged()
+    {
+        return $this->should_be_logged;
+    }
 }
