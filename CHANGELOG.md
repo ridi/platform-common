5001ede50b998a8a210ca0f242a74cceaadfad88
문정기 <jerrym@ridi.com>
2019-09-18 10:54:41 +0900

    ## [0.9.0] - 2019-09-18 ### Changed - Change Variable Type of `connection_pool` from `Doctrine/Connection` to `PDO base` for compatibility of usage ### Removed - Remove Function `getConnectionWithAutoReconnection` in `GnfConnectionProvider`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [0.9.0] - 2019-09-18
+### Changed
+- Change Variable Type of `connection_pool` from `Doctrine/Connection` to `PDO base` for compatibility of usage
+### Removed
+- Remove Function `getConnectionWithAutoReconnection` in `GnfConnectionProvider`
+
 ## [0.8.1] - 2019-09-17
 ### Added
 - Added Function `renameFile` in `FileUtils`
--- lib/DB/GnfConnectionProvider.php
+++ lib/DB/GnfConnectionProvider.php
@@ -12,7 +12,7 @@ use Ridibooks\Platform\Common\Cache\AdaptableCache;
 class GnfConnectionProvider
 {
     /**
-     * @var \Doctrine\DBAL\Connection[]
+     * @var base[]
      */
     private static $connection_pool = [];
 
@@ -25,34 +25,10 @@ class GnfConnectionProvider
     public static function getConnection($group_name)
     {
         if (!isset(self::$connection_pool[$group_name])) {
-            self::$connection_pool[$group_name] = self::createConnection($group_name);
+            self::$connection_pool[$group_name] = new PDO(self::createConnection($group_name));
         }
 
-        return new PDO(self::$connection_pool[$group_name]);
-    }
-
-    /**
-     * @param      $group_name
-     * @param bool $is_auto_reconnect
-     *
-     * @return Connection
-     * @throws \Doctrine\DBAL\DBALException
-     */
-    public static function getConnectionWithAutoReconnection($group_name, $is_auto_reconnect = false)
-    {
-        if (!isset(self::$connection_pool[$group_name])) {
-            self::$connection_pool[$group_name] = self::createConnection($group_name);
-        }
-
-        $connection = self::$connection_pool[$group_name];
-
-        // 접속 보장을 해야하는 경우 핑을 확인
-        if ($is_auto_reconnect && $connection->ping() === false) {
-            $connection->close();
-            $connection->connect();
-        }
-
-        return $connection;
+        return self::$connection_pool[$group_name];
     }
 
     /**
