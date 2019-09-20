07999865f8c4c55f801f22561dd65c101c913b94
문정기 <jerrym@ridi.com>
2019-09-20 10:13:25 +0900

    Add function transferFile

--- lib/AWS/S3Service.php
+++ lib/AWS/S3Service.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace Ridibooks\Platform\Common\AWS;
 
 use Aws\Credentials\CredentialProvider;
+use Aws\S3\Exception\S3Exception;
 use Aws\S3\S3Client;
+use Aws\S3\Transfer;
 use Ridibooks\Platform\Common\AWS\Dto\AwsConfigDto;
 
 class S3Service extends AbstractAwsService
@@ -43,5 +45,23 @@ class S3Service extends AbstractAwsService
         $default = stream_context_get_options(stream_context_get_default());
         $default['s3']['ACL'] = $default['s3']['acl'] = 'private';
         stream_context_set_default($default);
+    }
+
+    public function transferFile(string $s3_path, string $dest_path): bool
+    {
+        if (!is_dir($dest_path)) {
+            @mkdir($dest_path, 0777, true);
+        }
+
+        try {
+            $manager = new Transfer($this->client, $s3_path, $dest_path);
+            $manager->transfer();
+        } catch (S3Exception $se) {
+            return false;
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
     }
 }
