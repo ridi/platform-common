4617cd6633a4e78201ba9b439e49d9add6c1bfb7
kobi97 <kobi97@ridi.com>
2019-10-31 15:19:28 +0900

    add SignalManager 0.11.15

---
+++ lib/System/SignalManager.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ridibooks\Platform\Common\System;
+
+class SignalManager
+{
+    /** @var string pcntl_signal의 handler */
+    public const PCNTL_SIGNAL_HANDLER = 'Ridibooks\Platform\Common\System\SignalManager::signalHandler';
+
+    /** @var bool */
+    private static $CONTINUE = true;
+
+    public static function signalHandler(int $signo): void
+    {
+        if ($signo === SIGTERM) {
+            self::$CONTINUE = false;
+        }
+    }
+
+    public static function isContinued(): bool
+    {
+        return self::$CONTINUE;
+    }
+}
