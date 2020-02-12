abb9d89d49a0c64603944347bcc4f97c76b384c0
문정기 <jerrym@ridi.com>
2020-02-12 16:39:56 +0900

    ## [0.17.0] - 2020-02-12 ## Added - Add `SsmService` in AWS - Add Functions `getParameter`, `getParameterAsMap`, `setParameter` and `setParameterFromMap` in `SsmService`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [0.17.0] - 2020-02-12
+## Added
+- Add `SsmService` in AWS
+- Add Functions `getParameter`, `getParameterAsMap`, `setParameter` and `setParameterFromMap` in `SsmService`
+
 ## [0.16.8] - 2020-02-10
 ### Changed
 - Remove `WaitTimeSeconds` property in `SqsService::receiveMessages`
---
+++ lib/AWS/SsmService.php
@@ -0,0 +1,112 @@
+<?php
+declare(strict_types=1);
+
+namespace Ridibooks\Platform\Common\AWS;
+
+use Aws\Exception\AwsException;
+use Aws\Ssm\SsmClient;
+use Ridibooks\Platform\Common\Util\SentryHelper;
+
+/**
+ * @property SsmClient $client
+ */
+class SsmService extends AbstractAwsService
+{
+    public const TYPE_STRING = 'String';
+    public const TYPE_STRING_LIST = 'StringList';
+    public const TYPE_SECURE_STRING = 'SecureString';
+
+
+    protected function getAwsClass(): string
+    {
+        return SsmClient::class;
+    }
+
+    public function getParameter(string $name): array
+    {
+        $params = [
+            'Name' => $name,
+        ];
+
+        try {
+            $result = $this->client->getParameter($params);
+            $result = $result->get('Parameter');
+
+            $param_string = $result['Value'];
+            $raw_params = array_filter(explode(PHP_EOL, $param_string));
+
+            return $raw_params;
+        } catch (AwsException $e) {
+            SentryHelper::triggerSentryMessage(
+                'Fail To get parameters from SSM :' . $name . PHP_EOL
+                . 'Reason : ' . PHP_EOL
+                . $e->getMessage()
+            );
+            throw $e;
+        }
+    }
+
+    public function getParameterAsMap(string $name): array
+    {
+        try {
+            $raw_params = $this->getParameter($name);
+
+            $params = [];
+            foreach ($raw_params as $raw_param) {
+                $param = explode('=', $raw_param);
+                $value = $param[1];
+                if (is_numeric($value)) {
+                    $value = (int)$value;
+                } elseif (in_array($value, ['true', 'false'])) {
+                    $value = (bool)$value;
+                }
+
+                $params[$param[0]] = $value;
+            }
+
+            return $params;
+        } catch (AwsException $e) {
+            throw $e;
+        }
+    }
+
+    public function setParameter(string $name, array $params, string $type = self::TYPE_STRING): void
+    {
+        $value = implode(PHP_EOL, $params);
+
+        $this->putParameter($name, $value, $type);
+    }
+
+    public function setParameterFromMap(string $name, array $params, string $type = self::TYPE_STRING): void
+    {
+        $value_string = '';
+        foreach ($params as $key => $value) {
+            if (is_bool($value)) {
+                $value = $value ? 'true' : 'false';
+            }
+            $value_string .= $key . '=' . $value . PHP_EOL;
+        }
+
+        $this->putParameter($name, $value_string, $type);
+    }
+
+    private function putParameter(string $name, string $value, string $type): void
+    {
+        $params = [
+            'Type' => $type,
+            'Name' => $name,
+            'Value' => $value,
+        ];
+
+        try {
+            $result = $this->client->putParameter($params);
+        } catch (AwsException $e) {
+            SentryHelper::triggerSentryMessage(
+                'Fail To put parameters to SSM :' . $name . PHP_EOL
+                . 'Reason : ' . PHP_EOL
+                . $e->getMessage()
+            );
+            throw $e;
+        }
+    }
+}
