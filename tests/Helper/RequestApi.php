29b75f9e3d6fe11fdb3055cfa8e26f37050584a5
js.park <js.park@ridi.com>
2020-04-27 18:29:26 +0900

    Add RequestApiTest

---
+++ tests/Helper/RequestApi.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Ridibooks\Platform\Common\Tests\Helper;
+
+use Ridibooks\Platform\Common\Super\AbstractRequestApi;
+
+class RequestApi extends AbstractRequestApi
+{
+}
