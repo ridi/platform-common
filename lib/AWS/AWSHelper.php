b5bde9a12c6ffa4e48354b9d02e77594b87c47e7
문정기 <jerrym@ridi.com>
2020-05-13 11:06:50 +0900

    add getAwsDefaultConfigDto function and update getAwsConfigDto to adding cache in AwsHelper

--- lib/AWS/AWSHelper.php
+++ lib/AWS/AWSHelper.php
@@ -4,14 +4,29 @@ declare(strict_types=1);
 namespace Ridibooks\Platform\Common\AWS;
 
 use Aws\Credentials\CredentialProvider;
+use Aws\DoctrineCacheAdapter;
+use Doctrine\Common\Cache\FilesystemCache;
 use Ridibooks\Platform\Common\AWS\Dto\AwsConfigDto;
 
 class AWSHelper
 {
-    public static function getAwsConfigDto(?string $access_key, ?string $secret_access_key): AwsConfigDto
+    public static function getAwsDefaultConfigDto(bool $is_cached = false): AwsConfigDto
     {
         $credential = CredentialProvider::defaultProvider();
-        $aws_config_dto = AwsConfigDto::importFromCredentials($credential);
+
+        if ($is_cached) {
+            $credential = CredentialProvider::cache(
+                $credential,
+                new DoctrineCacheAdapter(new FilesystemCache('/tmp/cache'))
+            );
+        }
+
+        return AwsConfigDto::importFromCredentials($credential);
+    }
+
+    public static function getAwsConfigDto(?string $access_key, ?string $secret_access_key, bool $is_cached = false): AwsConfigDto
+    {
+        $aws_config_dto = self::getAwsDefaultConfigDto($is_cached);
 
         if (!empty($access_key) && !empty($secret_access_key)) {
             $aws_config_dto = AwsConfigDto::importFromAccessKeys($access_key, $secret_access_key);
